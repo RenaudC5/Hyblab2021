179f897398f42a925a005482825ab617a9628495
Guillaume MOLES <guillaume.moles@etu.univ-nantes.fr>
2021-01-19 18:45:51 +0100

    Création du fichier qui va gérer le json des parcs + implémentation des filtres permettant d'update le nombre de critère en commun avec ceux choisis

--- parents-b/public/js/data.js
+++ parents-b/public/js/data.js
@@ -1,6 +1,7 @@
-let json = require('../data/jardins.json');
-//console.log(json);
+let json = require('../data/jardins');
+console.log(json);
 
+/*
 json.forEach(x => x.nbElemCorrect = 0);
 
 let nbCritereTraite = 0;
@@ -14,7 +15,6 @@ nbCritereTraite++;
 json.sort((a, b) => { return b.nbElemCorrect - a.nbElemCorrect });
 
 console.log(json);
-/*
 json.filter(x => x["Nom"]);
 json.filter(x => x["Type"]);
 json.filter(x => x["Adresses"]);
@@ -23,11 +23,7 @@ json.filter(x => x["Géolocalisation"]);
 json.filter(x => x["Code postal"]);
 json.filter(x => x["Web"]);
 
-
-
-
-
-
+// Information complémentaire
 
 json.filter(x => x["Informations complémentaires"]);
 
@@ -35,7 +31,7 @@ json.forEach(x => {
 	if (x["Informations complémentaires"]) x.nbElemCorrect++;
 });
 
-
+// Gardien
 
 json.filter(x => x["Gardien"]);
 
@@ -43,7 +39,7 @@ json.forEach(x => {
 	if (x["Gardien"]) x.nbElemCorrect++;
 });
 
-
+// Jeux pour enfants
 
 json.filter(x => x["Jeux pour enfants"]);
 
@@ -51,7 +47,7 @@ json.forEach(x => {
 	if (x["Jeux pour enfants"]) x.nbElemCorrect++;
 });
 
-
+// Pataugeoire
 
 json.filter(x => x["Pataugeoire"]);
 
@@ -59,7 +55,7 @@ json.forEach(x => {
 	if (x["Pataugeoire"]) x.nbElemCorrect++;
 });
 
-
+// Sanitaires
 
 json.filter(x => x["Sanitaires"]);
 
@@ -67,7 +63,7 @@ json.forEach(x => {
 	if (x["Sanitaires"]) x.nbElemCorrect++;
 });
 
-
+// Sanitaires pour handicapés
 
 json.filter(x => x["Sanitaires pour handicapés"]);
 
@@ -75,7 +71,7 @@ json.forEach(x => {
 	if (x["Sanitaires pour handicapés"]) x.nbElemCorrect++;
 });
 
-
+// Jardin clos
 
 json.filter(x => x["Jardin clos"]);
 
@@ -83,8 +79,7 @@ json.forEach(x => {
 	if (x["Jardin clos"]) x.nbElemCorrect++;
 });
 
-
-
+// Abris
 
 json.filter(x => x["Abris"]);
 
@@ -92,10 +87,7 @@ json.forEach(x => {
 	if (x["Abris"]) x.nbElemCorrect++;
 });
 
-
-
-
-
+// Point d'eau
 
 json.filter(x => x["Point d'eau"]);
 
@@ -103,9 +95,7 @@ json.forEach(x => {
 	if (x["Point d'eau"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Table de pique-nique
 
 json.filter(x => x["Table pique-nique"]);
 
@@ -113,9 +103,7 @@ json.forEach(x => {
 	if (x["Table pique-nique"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Accès transports en commun
 
 json.filter(x => x["Accès transports en commun"]);
 
@@ -123,9 +111,7 @@ json.forEach(x => {
 	if (x["Accès transports en commun"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Bancs
 
 json.filter(x => x["Bancs"]);
 
@@ -133,7 +119,7 @@ json.forEach(x => {
 	if (x["Bancs"]) x.nbElemCorrect++;
 });
 
-
+// Accès Parking
 
 json.filter(x => x["Accès Parking"]);
 
@@ -141,9 +127,7 @@ json.forEach(x => {
 	if (x["Accès Parking"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Restauration
 
 json.filter(x => x["Restauration"]);
 
@@ -151,9 +135,7 @@ json.forEach(x => {
 	if (x["Restauration"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Présence d'animaux
 
 json.filter(x => x["Présence d'animaux"]);
 
@@ -161,9 +143,7 @@ json.forEach(x => {
 	if (x["Présence d'animaux"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Herbe/sable
 
 json.filter(x => x["Herbe (un minimum) / Sable"]);
 
@@ -171,9 +151,7 @@ json.forEach(x => {
 	if (x["Herbe (un minimum) / Sable"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Verdure / Espace vert
 
 json.filter(x => x["Verdure / Plante Espace Vert"]);
 
@@ -181,10 +159,7 @@ json.forEach(x => {
 	if (x["Verdure / Plante Espace Vert"]) x.nbElemCorrect++;
 });
 
-
-
-
-
+// CRAPA
 
 json.filter(x => x["CRAPA"]);
 
@@ -192,10 +167,7 @@ json.forEach(x => {
 	if (x["CRAPA"]) x.nbElemCorrect++;
 });
 
-
-
-
-
+// Terrains de sport
 
 json.filter(x => x["Terrains de sport"]);
 
@@ -203,9 +175,7 @@ json.forEach(x => {
 	if (x["Terrains de sport"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Activités organisées
 
 json.filter(x => x["Activités organisées"]);
 
@@ -213,9 +183,7 @@ json.forEach(x => {
 	if (x["Activités organisées"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Element de culture
 
 json.filter(x => x["Élément de culture"]);
 
@@ -223,235 +191,18 @@ json.forEach(x => {
 	if (x["Élément de culture"]) x.nbElemCorrect++;
 });
 
-
-
-
+// Horaires d'ouverture
 
 // TODO Définir la détection des horraires
 json.filter(x => x["Horaires d'ouverture"]);
 
+// Age
 
 // TODO Avoir une gestion de l'âge par rapport à l'écart à la tranche d'âge
 json.filter(x => x["Âge"]);
 
-// TODO Définir Qu'est ce qu'un grand/moyen/petit parc
-
-json.filter(x => x["Taille (m^2)"]);
-
-*/
-
-
-
-
-
-
-json.filter(x => x["Informations complémentaires"]);
-
-json.forEach(x => {
-    if (x["Informations complémentaires"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Gardien"]);
-
-json.forEach(x => {
-    if (x["Gardien"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Jeux pour enfants"]);
-
-json.forEach(x => {
-    if (x["Jeux pour enfants"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Pataugeoire"]);
-
-json.forEach(x => {
-    if (x["Pataugeoire"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Sanitaires"]);
-
-json.forEach(x => {
-    if (x["Sanitaires"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Sanitaires pour handicapés"]);
-
-json.forEach(x => {
-    if (x["Sanitaires pour handicapés"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Jardin clos"]);
-
-json.forEach(x => {
-    if (x["Jardin clos"]) x.nbElemCorrect++;
-});
-
-
-
-
-json.filter(x => x["Abris"]);
-
-json.forEach(x => {
-    if (x["Abris"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-
-json.filter(x => x["Point d'eau"]);
-
-json.forEach(x => {
-    if (x["Point d'eau"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Table pique-nique"]);
-
-json.forEach(x => {
-    if (x["Table pique-nique"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Accès transports en commun"]);
-
-json.forEach(x => {
-    if (x["Accès transports en commun"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Bancs"]);
-
-json.forEach(x => {
-    if (x["Bancs"]) x.nbElemCorrect++;
-});
-
-
-
-json.filter(x => x["Accès Parking"]);
-
-json.forEach(x => {
-    if (x["Accès Parking"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Restauration"]);
-
-json.forEach(x => {
-    if (x["Restauration"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Présence d'animaux"]);
-
-json.forEach(x => {
-    if (x["Présence d'animaux"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Herbe (un minimum) / Sable"]);
-
-json.forEach(x => {
-    if (x["Herbe (un minimum) / Sable"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Verdure / Plante Espace Vert"]);
-
-json.forEach(x => {
-    if (x["Verdure / Plante Espace Vert"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-
-json.filter(x => x["CRAPA"]);
-
-json.forEach(x => {
-    if (x["CRAPA"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-
-json.filter(x => x["Terrains de sport"]);
-
-json.forEach(x => {
-    if (x["Terrains de sport"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Activités organisées"]);
-
-json.forEach(x => {
-    if (x["Activités organisées"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-json.filter(x => x["Élément de culture"]);
-
-json.forEach(x => {
-    if (x["Élément de culture"]) x.nbElemCorrect++;
-});
-
-
-
-
-
-// TODO Définir la détection des horraires
-json.filter(x => x["Horaires d'ouverture"]);
-
-
-// TODO Avoir une gestion de l'âge par rapport à l'écart à la tranche d'âge
-json.filter(x => x["Âge"]);
+// Taille
 
 // TODO Définir Qu'est ce qu'un grand/moyen/petit parc
-
 json.filter(x => x["Taille (m^2)"]);
-
 */
