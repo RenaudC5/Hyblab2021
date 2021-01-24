65b8474509e6350880b8b8d8ea123d21db6f51e6
E177176H <thibaut.perrouin@etu.univ-nantes.fr>
2021-01-24 22:34:27 +0100

    Sliding horizontal et vertical entre les pages + fade de la page results vers la page home

--- parents-b/public/js/agePage.js
+++ parents-b/public/js/agePage.js
@@ -1,6 +1,6 @@
 let initAge = function(){
-    d3.select(".button-next-age").on("click", function (){
-        mySlidr.slide("access-page");
+    d3.select('.button-next-age').on('click', function (){
+        mySlidr.slide('right');
         initAccess();
     });
 };
