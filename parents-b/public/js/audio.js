3c370a639716355290835baca8087f4fdabc7ab1
Camille <camille.barthez@etu.univ-nantes.fr>
2021-01-30 16:02:02 +0100

    son part1

--- parents-b/public/js/audio.js
+++ parents-b/public/js/audio.js
@@ -1,13 +1,1 @@
-
-    d3.select('.volume-on').on('click', function (){
-        //Doit enlever le son
-    });
-
-  //------------AUDIO-----------
-  document.addEventListener('click', musicPlay);
-  function musicPlay() {
-      document.getElementById('horaire_audio').play();
-      document.getElementById('horaire_audio').loop = false;
-      document.removeEventListener('click', musicPlay);
-  }
-
+var isSonOn = Boolean(true);
--- parents-b/public/js/searchingResultsPage.js
+++ parents-b/public/js/searchingResultsPage.js
@@ -1,4 +1,30 @@
 let initSearchingResults = function(){
+    //-----------AUDIO--------------
+    document.getElementById('activite_audio').pause();
+
+    let buutonVol = document.getElementById("volumeResult1");
+    buutonVol.setAttribute("src", "./img/common/volume_on.svg");
+
+    if(isSonOn){
+        document.getElementById('result1_audio').play();
+        document.getElementById('result1_audio').loop = false;
+    }
+    else{
+        buutonVol.setAttribute("src", "./img/common/volume_off.svg");
+    }
+    d3.selectAll('.volume').on('click', function (){
+        if(isSonOn){
+            this.setAttribute("src", "./img/common/volume_off.svg");
+            isSonOn = Boolean(false);
+            document.getElementById('result1_audio').pause(); 
+        }
+        else{
+            this.setAttribute("src", "./img/common/volume_on.svg");
+            isSonOn = Boolean(true);
+            document.getElementById('result1_audio').play(); 
+        }
+    }); 
+
     setTimeout(function(){
         mySlidr.slide('down');
         initResults();
