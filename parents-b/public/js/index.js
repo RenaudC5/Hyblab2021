b7e3f8b30b7cb1c22985fd83db416836b8b24732
Colin RENAUD <colin.renaud5@gmail.com>
2021-01-30 12:29:55 +0100

    fix bug slidr

--- parents-b/public/js/index.js
+++ parents-b/public/js/index.js
@@ -8,8 +8,7 @@ let mySlidr = slidr.create('slidr',{
     timing: { 'linear': '1.5s ease-out' }
 })
 
-    .add('v', ['home-page','address-page','more-infos-page', 'home-page'], 'linear')
-    .add('v', ['home-page', 'address-page', 'home-page'], 'linear')
+    .add('v', ['home-page','address-page','more-infos-page','home-page'], 'linear')
     .add('h', ['address-page', 'hour-page', 'age-page', 'access-page', 'address-page'], 'linear')
     .add('v', ['access-page', 'fauna-flora-page', 'access-page'], 'linear')
     .add('h', ['fauna-flora-page', 'activities-page', 'searching-results-page', 'fauna-flora-page'], 'linear')
--- parents-b/public/js/more_infos.js
+++ parents-b/public/js/more_infos.js
@@ -1,6 +1,7 @@
 let initMoreInfo = function(){
     d3.select('#back-menu').on('click', function (){
         mySlidr.slide('down');
+        mySlidr.slider("value", mySlidr.slider("option", "min") );
         initHome();
     });
 }
