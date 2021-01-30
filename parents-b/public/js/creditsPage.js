3916b877c154306d58186f161811c143da7890b5
Loïc BOURSIN <loic.boursin@etu.univ-nantes.fr>
2021-01-30 17:47:15 +0100

    améliorations

--- parents-b/public/js/creditsPage.js
+++ parents-b/public/js/creditsPage.js
@@ -1,8 +1,10 @@
 let initCredits = function(){
 
-    d3.select('.go-back-end').on('click', function (){
+    d3.select('.go-back-end').on('click', function () {
         mySlidr.slide('left');
         initEnd();
+    });
+
     d3.select('.more-info').on('click', function (){
         //Doit ouvrir la page qui sommes nous
     });
